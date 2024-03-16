3363c391248fe0e5ef1224fd3ed9b43e1c519198
Di Qu <diqu97@gmail.com>
2024-03-15 23:55:46 -0700

    Fix: Fix bracket typo and naming issue

--- C++/Lecture 3, OOP Refactor/TCPServer.cpp
+++ C++/Lecture 3, OOP Refactor/TCPServer.cpp
@@ -6,43 +6,43 @@ TCPServer::TCPServer(int domain) {
         std::cerr << "Failed to create a socket." << std::endl;
         return;
     }
+}
 
-    TCPServer::~TCPServer() {
-        close(serverSocket_);
-    }
+TCPServer::~TCPServer() {
+    close(serverSocket_);
+}
 
-    TCPServer::bind(int domain, int port) {
-        struct sockaddr_in server_addr;
-        server_addr.sin_family = domain;
-        server_addr.sin_addr.s_addr = INADDR_ANY;
-        server_addr.sin_port = htons(port);
+int TCPServer::server_bind(int domain, int port) {
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = domain;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
 
-        if (bind(serverSocket_, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-            std::cerr << "Failed to bind the socket to the address." << std::endl;
-            return -1;
-        }
-    }
-
-    TCPServer::listen(int backlog) {
-        if (listen(serverSocket_, backlog) == -1) {
-            std::cerr << "Failed to listen for incoming connections." << std::endl;
-            return -1;
-        }
-    }
-
-    TCPServer::accept() {
-        struct sockaddr_in server_addr;
-        int addrlen = sizeof(server_addr);
-        int clientSocket = accept(serverSocket_, (struct sockaddr*)&server_addr, (socklen_t*)&addrlen);
-        if (clientSocket == -1) {
-            std::cerr << "Failed to accept the connection." << std::endl;
-            return -1;
-        }
-        clientSockets_.push_back(clientSocket);
-        return clientSocket;
-    }
-
-    TCPServer::send(int fd, const char* buffer, int length) {
-        send(fd, buffer, length, 0);
+    if (bind(serverSocket_, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Failed to bind the socket to the address." << std::endl;
+        return -1;
     }
 }
+
+int TCPServer::server_listen(int backlog) {
+    if (listen(serverSocket_, backlog) == -1) {
+        std::cerr << "Failed to listen for incoming connections." << std::endl;
+        return -1;
+    }
+}
+
+int TCPServer::server_accept() {
+    struct sockaddr_in server_addr;
+    int addrlen = sizeof(server_addr);
+    int clientSocket = accept(serverSocket_, (struct sockaddr*)&server_addr, (socklen_t*)&addrlen);
+    if (clientSocket == -1) {
+        std::cerr << "Failed to accept the connection." << std::endl;
+        return -1;
+    }
+    clientSockets_.push_back(clientSocket);
+    return clientSocket;
+}
+
+int TCPServer::server_send(int fd, const char* buffer, int length) {
+    send(fd, buffer, length, 0);
+} 
